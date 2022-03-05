3cf81f8943e4c2b61b338aee7b8ea0803ed08e09
erka <erdenebold10@gmail.com>
2022-03-05 23:30:14 +0800

    add code

---
+++ bsl_code_update.sh
@@ -0,0 +1,8 @@
+cd /opt/odoo/data_center_web
+sudo git pull
+sudo /etc/init.d/odoo-server stop
+sudo /etc/init.d/odoo-server stop
+sudo /etc/init.d/odoo-server stop
+sudo /etc/init.d/odoo-server stop
+sudo /etc/init.d/odoo-server stop
+sudo /etc/init.d/odoo-server start
